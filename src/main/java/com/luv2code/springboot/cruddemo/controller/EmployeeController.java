46dca18210cdbd23b84eb2da2a6a0f3f1e16a5b6
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-16 10:27:03 -0300

    add update button and create end poit to edit the employee and update

--- src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
@@ -35,6 +36,15 @@ public class EmployeeController {
 	public String showFormForAdd(Model model) {
 
 		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+
+		return "employees/employee-form";
+	}
+
+	@GetMapping("/showFormUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+
+		Employee employee = this.employeeService.findById(id);
 		model.addAttribute("employee", employee);
 
 		return "employees/employee-form";
